6a705da214d05a4e2f5f5ed25d159a40b7368c67
fmchan <formingchan@gmail.com>
2022-01-21 10:33:55 +0800

    - build docker image with no cache and pull new version - expose ports with scale - config server health not work

--- buildDockerImage.bat
+++ buildDockerImage.bat
@@ -1,5 +1,5 @@
 @echo off
 
 for %%i in (admin-dashboard config-server eureka-server hystrix-dashboard service-a service-b zuul) do (
-     docker build -t "spring-cloud-netflix-example/%%i:latest" %%i
+     docker build -t "spring-cloud-netflix-example/%%i:latest" %%i --no-cache --pull
 )
